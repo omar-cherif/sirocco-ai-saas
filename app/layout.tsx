16105676ad38ccd454f96e9fb9a52f91fa79930b
Omar CHERIF <omar.cherif@gmail.com>
2023-12-06 15:49:29 +0100

    import Script from next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ToasterProvider } from '@/components/toaster-provider'
 import { ModalProvider } from '@/components/modal-provider'
 import { CrispProvider } from '@/components/crisp-provider'
 
+import Script from 'next/script'
+
 import './globals.css'
 
 const font = Inter({ subsets: ['latin'] });
@@ -23,7 +25,30 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <CrispProvider />
+      <Script
+          strategy='lazyOnload'
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+
+        <Script id='google-analytics' strategy='lazyOnload'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+          `}
+        </Script>
         <body className={font.className}>
           <ToasterProvider />
           <ModalProvider />
