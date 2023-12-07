022af2dd287d0e6e73442a8b2b96f0eee93c19d1
Omar CHERIF <omar.cherif@gmail.com>
2023-12-07 19:30:52 +0100

    remove script (analiytics) from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,30 +25,6 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-      <Script
-          strategy='lazyOnload'
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        />
-
-        <Script id='google-analytics' strategy='lazyOnload'>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-            page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-        <Script id='google-tag-manager' strategy='afterInteractive'>
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
-          `}
-        </Script>
         <body className={font.className}>
           <ToasterProvider />
           <ModalProvider />
